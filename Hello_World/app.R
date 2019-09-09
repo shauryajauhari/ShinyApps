454c95aa6207922ea60e7d00dfff613c9ad44671
Shaurya Jauhari <shaurya126906@st.jmi.ac.in>
2019-09-09 08:39:54 +0800

    Update code

--- Hello_World/app.R
+++ Hello_World/app.R
@@ -21,32 +21,50 @@
 
 ## Program3
 
-ui <- fluidPage( h1("Hello World!"), ## Using h1/ heading tag from HTML
-                 sidebarLayout(sidebarPanel(),
-                               mainPanel())
-                 ) 
-server <- function(input, output, session) {
-
-}
-
-shinyApp(ui, server)
+# ui <- fluidPage( h1("Hello World!"), ## Using h1/ heading tag from HTML
+#                  sidebarLayout(sidebarPanel(),
+#                                mainPanel())
+#                  ) 
+# server <- function(input, output, session) {
+# 
+# }
+# 
+# shinyApp(ui, server)
 
 ## Program4
 
-ui <- fluidPage( 
-  h1("Hello World!"), ## Using h1/ heading tag from HTML
-                 sidebarLayout(
-                   sidebarPanel(
-                     selectInput("dataset", "Choose a dataset:",
-                                 choices = ls ("package:datasets"),
-                                 selected = "pressure")
-                   ),
-                   mainPanel()
-                   )
+# ui <- fluidPage( 
+#   h1("Hello World!"), ## Using h1/ heading tag from HTML
+#                  sidebarLayout(
+#                    sidebarPanel(
+#                      selectInput("dataset", "Choose a dataset:",
+#                                  choices = ls ("package:datasets"),
+#                                  selected = "pressure")
+#                    ),
+#                    mainPanel()
+#                    )
+# ) 
+# server <- function(input, output, session) {
+#   
+# }
+# 
+# shinyApp(ui, server)
+
+
+ui <- fluidPage(
+  titlePanel(title = "Welcome to Shaurya's Blog."),
+  h4("This application is for getting familiar with the basic concepts of 
+     Shiny. To begin with, let's explore some datasets bundled with R."),
+  sidebarLayout(
+    sidebarPanel(),
+    mainPanel()
+    )
 ) 
 server <- function(input, output, session) {
+  plot()
   
 }
 
 shinyApp(ui, server)
+
 
